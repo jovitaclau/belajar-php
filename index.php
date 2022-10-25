f6d2e8c754ec729984eb74a486f4d1a3cced7b1a
Jovita Claudia Winata <jvtcld@gmail.com>
2022-10-25 17:50:05 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Jovita";
+$umur = 20;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaKakak = "Joanne";
+$umurKakak = 21;
+
+echo "Nama kakak saya <strong>$namaKakak</strong>, kakak saya berusia $umurKakak tahun.<br>";
+
+$selisihUmur = $umurKakak - $umur;
+
+echo "Selisih usia kami adalah $selisihUmur tahun."
+
+
+?>
